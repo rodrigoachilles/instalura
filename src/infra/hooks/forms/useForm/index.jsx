40db8b02ed43a465aaeb0f757092e8dd6fea0277
Rodrigo Achilles <rodrigo.achilles@serpro.gov.br>
2021-05-07 17:07:50 -0300

    test: testes de Hooks e Service
    Criação dos testes do hooks do form e do login service

--- src/infra/hooks/forms/useForm/index.jsx
+++ src/infra/hooks/forms/useForm/index.jsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from 'react';
 
+function formatErrors(yupErrorsInner = []) {
+  return yupErrorsInner.reduce((errorObjectAcc, currentError) => {
+    const fieldName = currentError.path;
+    const errorMessage = currentError.message;
+    return {
+      ...errorObjectAcc,
+      [fieldName]: errorMessage,
+    };
+  }, {});
+}
 
 export default function useForm({
   initialValues,
@@ -17,21 +27,17 @@ export default function useForm({
       setErrors({});
       setIsFormDisabled(false);
     } catch (err) {
-      const formatedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
-        const fieldName = currentError.path;
-        const errorMessage = currentError.message;
-        return {
-          ...errorObjectAcc,
-          [fieldName]: errorMessage,
-        };
-      }, {});
+      const formatedErrors = formatErrors(err.inner);
       setErrors(formatedErrors);
       setIsFormDisabled(true);
     }
   }
 
   useEffect(() => {
-    validateValues(values);
+    validateValues(values)
+      .catch((err) => {
+        console.log(err);
+      });
   }, [values]);
 
   return {
--- src/services/login/loginService.jsx
+++ src/services/login/loginService.jsx
@@ -24,8 +24,10 @@ const BASE_URL = isStagingEnv
   : 'https://instalura-api.omariosouto.vercel.app';// Back End de PROD
 
 const loginService = {
-  async login({ username, password }) {
-    return HttpClient(`${BASE_URL}/api/login`, {
+  async login({ username, password },
+    setCookieModule = setCookie,
+    HttpClientModule = HttpClient) {
+    return HttpClientModule(`${BASE_URL}/api/login`, {
       method: 'POST',
       body: {
         username, // 'omariosouto'
@@ -34,9 +36,13 @@ const loginService = {
     })
       .then((respostaConvertida) => {
         const { token } = respostaConvertida.data;
+        const hasToken = token;
+        if (!hasToken) {
+          throw new Error('Failed to login');
+        }
         const DAY_IN_SECONDS = 86400;
 
-        setCookie(null, 'APP_TOKEN', token, {
+        setCookieModule(null, 'APP_TOKEN', token, {
           path: '/',
           maxAge: DAY_IN_SECONDS * 7,
         });
@@ -46,8 +52,8 @@ const loginService = {
         };
       });
   },
-  logout() {
-    destroyCookie(null, 'APP_TOKEN');
+  async logout(destroyCookieModule = destroyCookie) {
+    destroyCookieModule(null, 'APP_TOKEN');
   },
 };
 
